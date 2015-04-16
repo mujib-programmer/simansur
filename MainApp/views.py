58bba4db722005d3c627787ea7021b9db7fd57fd
PHPGeek Programmer <mujib.programmer@gmail.com>
2015-04-16 15:29:44 +0700

    frontend using twitter bootstrap. Completely working with static data.

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,3 +1,23 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context_dict = {'slug': 'login'}
+
+    return render(request, 'MainApp/index.html', context_dict)
+
+def surat(request):
+    context_dict = {}
+
+    return render(request, 'MainApp/surat.html', context_dict)
+
+def user(request):
+    context_dict = {}
+
+    return render(request, 'MainApp/user.html', context_dict)
+
+def login(request):
+    context_dict = {'slug': 'login'}
+
+    return render(request, 'MainApp/login.html', context_dict)
--- simansur/urls.py
+++ simansur/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from MainApp import views
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'simansur.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^$', views.index, name='home'),
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^surat/$', views.surat, name='surat'),
+    url(r'^user/$', views.user, name='user'),
 )
